0eb73dfc474f6bd39913f2191b65e8b2637418fd
James Stevenson <james.stevenson09@hotmail.co.uk>
2017-10-14 17:43:49 +0100

    changed header on bio

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ include "./header_nav.php";
 		<img src="media/CatherineStevenson5.jpg" height="275px" width="275px"/>
 	</div>
     	<div class="col-md-4">
-		<h3>Bio</h3>
+		<h3>Biography</h3>
 			<p>Catherine Stevenson studied drawing and painting at Edinburgh College of Art from 1974 to 1978.</p>
 			<p>She was awarded the Professional Graduate Diploma from Moray House School of Education in 1982 and taught Art and Design for 33 years in secondary schools in West Lothian and Edinburgh, retiring early in order to paint full time.</p>
     	</div>
         <div class="col-md-4">
-        	<h3>Influences</h3>
+        	<h3>Artist's Statement</h3>
 			<p>Catherine works in a range of media including oil paint, acrylics and watercolour. In the Bell Jar series she aims to explore the theme of Time; creating carefully composed images that are both transparent and reflective, and using multiple layers of images, colour, pattern to represent both memories of the past and objects from the present.</p>
 			<p>She has recently completed a series of watercolour illustrations which have been included in 'A Sketchbook of Edinburgh' (published by Birlinn, 2017).</p>
         </div>
