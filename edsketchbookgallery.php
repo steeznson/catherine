5c53822f9ba1a4bb24a6aa2ddf192b9f9cae6fb1
steeznson <james.stevenson09@hotmail.co.uk>
2018-01-04 14:58:23 +0000

    added photography section

--- edsketchbookgallery.php
+++ edsketchbookgallery.php
@@ -6,52 +6,52 @@
 	<div class="row">
         <div class='list-group gallery'>
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/botanicsgateeastlight.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/botanicsgateeastlight.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/botanicsgateeastlight.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/stewartsmelville.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/stewartsmelville.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/stewartsmelville.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/gormleyman.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/gormleyman.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/gormleyman.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/inverleithhousenorth.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/inverleithhousenorth.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/inverleithhousenorth.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/inverleithhousesouth.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/inverleithhousesouth.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/inverleithhousesouth.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/rebuiltcottage.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/rebuiltcottage.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/rebuiltcottage.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/reflectivegate.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/reflectivegate.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/reflectivegate.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/sngma1.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/sngma1.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/sngma1.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/sngma2.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/sngma2.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/sngma2.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
         	<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-                   <a class="fancybox thumbnail" rel="ligthbox" href="media/thumbs/sketchbookedin/lilypads2.jpg">
+                   <a class="fancybox thumbnail" rel="ligthbox" href="media/sketchbookedin/lilypads2.jpg">
 			<img class="img-responsive" alt="" src="media/sketchbookedin/lilypads2.jpg" />
 		   </a>
              	</div> <!-- col-6 / end -->
