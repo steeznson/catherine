be2de510f87d7901a88339801bc07d0f6b093022
steeznson <james.stevenson09@hotmail.co.uk>
2018-01-16 11:10:00 +0000

    changed index page to reflect artist's wishes

--- index.php
+++ index.php
@@ -13,20 +13,23 @@ include "./header_nav.php";
 <div class="container">
 
   	<div class="row">
-    	<div class="col-md-4">
-      		<h3>The Artist</h3>                	
-		<img src="media/CatherineStevenson5.jpg" height="275px" width="275px"/>
-	</div>
-    	<div class="col-md-4">
-		<h3>Biography</h3>
-			<p>Catherine Stevenson studied drawing and painting at Edinburgh College of Art from 1974 to 1978.</p>
-			<p>She was awarded the Professional Graduate Diploma from Moray House School of Education in 1982 and taught Art and Design for 33 years in secondary schools in West Lothian and Edinburgh, retiring early in order to paint full time.</p>
-    	</div>
+  	
+  	<div class="col-md-1">
+  	</div>
+  	
         <div class="col-md-4">
-        	<h3>Artist's Statement</h3>
-			<p>Catherine works in a range of media including oil paint, acrylics and watercolour. In the Bell Jar series she aims to explore the theme of Time; creating carefully composed images that are both transparent and reflective, and using multiple layers of images, colour, pattern to represent both memories of the past and objects from the present.</p>
-			<p>She has recently completed a series of watercolour illustrations which have been included in <i>A Sketchbook of Edinburgh</i> (published by Birlinn, 2017).</p>
+			<p>Catherine works in a range of media including oil paint, acrylics and watercolour.<br/>
+			In the Bell Jar series she aims to explore the theme of Time; creating carefully composed images that are both transparent and reflective, and using multiple layers of images, colour, pattern to represent both memories of the past and objects from the present.<br/>
+			She has recently completed a series of watercolour illustrations which have been included in <i>A Sketchbook of Edinburgh</i> (published by Birlinn, 2017).</p>
         </div>
+        
+        <div class="col-md-2">
+        </div>
+        
+        <div class="col-md-5">
+        	<img src="./media/belljargreeter.jpg" width="320" height="320">
+        </div>
+  	
   	</div>
 
 </div>
