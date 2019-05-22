80798270e5c6f050f807090a972291bdab0477cb
James Stevenson <james.al.stevenson@gmail.com>
2019-05-22 19:43:04 +0100

    tidying up

--- belljargallery.php
+++ belljargallery.php
@@ -6,112 +6,85 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/5belljarflower2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt="" src="media/belljar/5belljarflower2.jpg" />
-          <span class="bottom"><i>Flower</i></span><
+          <img class="img-responsive " alt="" src="media/belljar/5belljarflower2.jpg" />
+          <span class="title"><i>Flower</i></span>
         </div>
       </a>      
     </div>  
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/6belljarelephant2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/6belljarelephant2.jpg" />               
-          <span class="bottom painting-title"><i>Elephant</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/6belljarelephant2.jpg"/>
+	  <span class="title"><i>Elephant</i></span>
         </div>
       </a>      
     </div>  
-    <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/7belljarduck2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/7belljarduck2.jpg" />
-          <span class="bottom painting-title"><i>Duck</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/7belljarduck2.jpg" />
+          <span class="title"><i>Duck</i></span>
         </div>
       </a>      
     </div>
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/4belljarhare2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/4belljarhare2.jpg" />
-          <span class="bottom painting-title"><i>Hare</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/4belljarhare2.jpg" />
+          <span class="title"><i>Hare</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/9belljarbluegoose2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/9belljarbluegoose2.jpg" />
-          <span class="bottom painting-title"><i>Blue Goose</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/9belljarbluegoose2.jpg" />
+          <span class="title"><i>Blue Goose</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/10belljatnetsuke.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/10belljatnetsuke.jpg" />
-          <span class="bottom painting-title"><i>Netsuke</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/10belljatnetsuke.jpg" />
+          <span class="title"><i>Netsuke</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/8belljardalicabinet2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt="" src="media/belljar/8belljardalicabinet2.jpg" />
-          <span class="bottom painting-title"><i>Dali Cabinet</i></span>
+          <img class="img-responsive " alt="" src="media/belljar/8belljardalicabinet2.jpg" />
+          <span class="title"><i>Dali Cabinet</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/2belljarskull2.jpg">   
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/2belljarskull2.jpg" />
-          <span class="bottom painting-title"><i>Skull</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/2belljarskull2.jpg" />
+          <span class="title"><i>Skull</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/1belljarspoon2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt=""  src="media/belljar/1belljarspoon2.jpg" />
-          <span class="bottom"><i>Spoon</i></span>
+          <img class="img-responsive " alt=""  src="media/belljar/1belljarspoon2.jpg" />
+          <span class="title"><i>Spoon</i></span>
         </div>
       </a>
-      
-      
     </div>
-    
-    <div class="col-md-3 col-sm-6 col-xs-12">
-      
+    <div class="col-md-4 col-sm-6 col-xs-7">
       <a class="fancybox thumbnail" rel="lightbox" href="media/belljar/3belljarhook2.jpg">
         <div class="image-container">
-          <img class="img-responsive painting" alt="" src="media/belljar/3belljarhook2.jpg" />
-          <span class="bottom"><i>Hook</i></span>
+          <img class="img-responsive " alt="" src="media/belljar/3belljarhook2.jpg" />
+          <span class="title"><i>Hook</i></span>
         </div>
-      </a>
-      
-      
+      </a>      
     </div>
   </div> 
 </div>
