cd3e2a25b76f474132f577784e6bc56c6ab7f63f
James Stevenson <james.al.stevenson@gmail.com>
2020-04-17 12:39:27 +0100

    170420-1

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
 <div class="container">
   <div class="row">
-  <img id="splashimg" class="center-block" src="media/collection/anapartmentinparis.jpeg" alt="splashimg">
+    <div class="card">
+      <h2 class="card-title">Catherine Stevenson - Artist</h2>
+      <p class="card-text">Catherine works in a range of media including oil paint, acrylics and watercolour. In the Bell Jar series she aims to explore the theme of Time; creating carefully composed images that are both transparent and reflective, and using multiple layers of images, colour, pattern to represent both memories of the past and objects from the present.</p>
+    </div>
   </div>
-  <div class="row">
-  <div class="card">
-    <h2 class="card-title">Catherine Stevenson - Artist</h2>
-    <p class="card-text">Catherine works in a range of media including oil paint, acrylics and watercolour. In the Bell Jar series she aims to explore the theme of Time; creating carefully composed images that are both transparent and reflective, and using multiple layers of images, colour, pattern to represent both memories of the past and objects from the present.</p>
-  </div>
+    <div class="row">
+    <img id="splashimg" class="center-block" src="media/collection/anapartmentinparis.jpeg" alt="splashimg">
   </div>
 </div>
 
