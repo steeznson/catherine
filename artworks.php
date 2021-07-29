6e78ef571ea99e193222b16e6a8d602dc6744ad7
Alex Dover <alexandradover@hotmail.com>
2021-07-29 11:46:41 +0100

    add lazy loading

--- artworks.php
+++ artworks.php
@@ -7,7 +7,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/portraits/milly.jpg">
         <div class="image-container">
-          <img class="img-responsive  " alt=""  src="media/portraits/milly.jpg" />
+          <img class="img-responsive lazy-image" alt=""  src="media/portraits/milly-lazy.jpg" data-src="media/portraits/milly.jpg" />
         </div>
       </a>
       <span><small><i>Portrait</i> - oil on board - 75x50cms</small></span>
@@ -15,7 +15,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/italianstilllife.jpeg">
         <div class="image-container">
-          <img class="img-responsive " alt=""  src="media/artworks/italianstilllife.jpeg"/>
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/italianstilllife-lazy.jpeg" data-src="media/artworks/italianstilllife.jpeg"/>
         </div>
       </a>
       <span><small><i>Italian Stilllife</i> - oil on board - 30x21cms</small></span>
@@ -23,7 +23,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/portraits/james.jpg">
         <div class="image-container">
-          <img class="img-responsive  " alt=""  src="media/portraits/james.jpg" />
+          <img class="img-responsive lazy-image" alt=""  src="media/portraits/james-lazy.jpg" data-src="media/portraits/james.jpg" />
         </div>
       </a>
       <span><small><i>Portrait</i> - acrylics - 75x50cms</small></span>
@@ -33,7 +33,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/redfruit.jpeg">
         <div class="image-container">
-          <img class="img-responsive " alt=""  src="media/artworks/redfruit.jpeg"/>
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/redfruit-lazy.jpeg" data-src="media/artworks/redfruit.jpeg" />
         </div>
       </a>
       <span><small><i>Red Fruit</i> - oil on board - 30x30cms</small></span>
@@ -41,7 +41,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/theboathouse.jpeg">
         <div class="image-container">
-          <img class="img-responsive " alt=""  src="media/artworks/theboathouse.jpeg"/>
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/theboathouse-lazy.jpeg" data-src="media/artworks/theboathouse.jpeg"/>
         </div>
       </a>
       <span><small><i>The Boat House at Loch Insh</i> - oil on board - 30x30cms</small></span>
@@ -49,22 +49,22 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/yellowpear.jpeg">
         <div class="image-container">
-          <img class="img-responsive " alt=""  src="media/artworks/yellowpear.jpeg"/>
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/yellowpear-lazy.jpeg" data-src="media/artworks/yellowpear.jpeg"/>
         </div>
       </a>
       <span><small><i>Yellow Pear</i> - oil on board - 30x30cms</small></span>
     </div>
   </div>
-  <div id="revealerDiv" class="col-sm-12 span">
+  <!-- <div id="revealerDiv" class="col-sm-12 span">
       <button id="revealer" type="button">
         <p class="glyphicon glyphicon-menu-down"></p> Next
       </button>
-  </div>
-  <div class="row hide">
+  </div> -->
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/autumnonmull.jpg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/autumnonmull.jpg"/>
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/autumnonmull-lazy.jpg" data-src="media/artworks/autumnonmull.jpg"/>
         </div>
       </a>
       <span><small><i>Autumn on Mull</i> - oil on board - 40x40cms</small></span>
@@ -72,7 +72,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/largemantelpiece.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/largemantelpiece.jpeg" />
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/largemantelpiece-lazy.jpeg" data-src="media/artworks/largemantelpiece.jpeg" />
         </div>
       </a>
       <span><small><i>Large Mantelpiece</i> - oil on board - 75x50cms</small></span>
@@ -80,17 +80,17 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/lochside.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/lochside.jpeg"/>
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/lochside-lazy.jpeg" data-src="media/artworks/lochside.jpeg"/>
         </div>
       </a>
       <span><small><i>Lochside</i> - oil on board - 40x40cms</small></span>
     </div>
   </div>
-  <div class="row hide">
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/mantelpiece.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/mantelpiece.jpeg"/>
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/mantelpiece-lazy.jpeg" data-src="media/artworks/mantelpiece.jpeg"/>
         </div>
       </a>
       <span><small><i>Mantelpiece</i> - oil on canvas - 50x40cms</small></span>
@@ -98,7 +98,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thecabinet3.jpg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thecabinet3.jpg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thecabinet3-lazy.jpg" data-src="media/artworks/thecabinet3.jpg" />
         </div>
       </a>
       <span><small><i>The Cabinet 3</i> - oil on board - 50x50cms</small></span>
@@ -106,17 +106,17 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thecabinet2.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thecabinet2.jpeg"/>
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thecabinet2-lazy.jpeg" data-src="media/artworks/thecabinet2.jpeg"/>
         </div>
       </a>
       <span><small><i>The Cabinet 2</i> - oil on board - 56x35cms</small></span>
     </div>
   </div>
-  <div class="row hide">
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thecollection.jpg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thecollection.jpg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thecollection-lazy.jpg" data-src="media/artworks/thecollection.jpg" />
         </div>
       </a>
       <span><small><i>The Collection</i> - oil on board - 40x40cms</small></span>
@@ -124,7 +124,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/cagedbird1_40x40.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/cagedbird1_40x40.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/cagedbird1_40x40-lazy.jpeg" data-src="media/artworks/cagedbird1_40x40.jpeg" />
         </div>
       </a>
       <span><small><i>Caged Bird 1</i> - oil on board - 40x40cms</small></span>
@@ -132,17 +132,17 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/cagedbird2_40x40.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/cagedbird2_40x40.jpeg" />
+          <img class="img-responsive lazy-image" alt=""src="media/artworks/cagedbird2_40x40-lazy.jpeg" data-src="media/artworks/cagedbird2_40x40.jpeg" />
         </div>
       </a>
       <span><small><i>Caged Bird 2</i> - oil on board - 40x40cms</small></span>
     </div>
   </div>
-  <div class="row hide">
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/cagedbird3_40x40.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/cagedbird3_40x40.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/cagedbird3_40x40-lazy.jpeg" data-src="media/artworks/cagedbird3_40x40.jpeg" />
         </div>
       </a>
       <span><small><i>Caged Bird 3</i> - oil on board - 40x40cms</small></span>
@@ -150,7 +150,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thecabinet.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thecabinet.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thecabinet-lazy.jpeg" data-src="media/artworks/thecabinet.jpeg" />
         </div>
       </a>
       <span><small><i>The Cabinet</i> - oil on board - 60x60cms</small></span>
@@ -158,17 +158,17 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/winterpath.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image  " alt=""  data-src="media/artworks/winterpath.jpeg" />
+          <img class="img-responsive lazy-image" alt=""  src="media/artworks/winterpath-lazy.jpeg" data-src="media/artworks/winterpath.jpeg" />
         </div>
       </a>
       <span><small><i>Winter Path</i> - oil on board - 40x40cms</small></span>
     </div>
   </div>
-  <div class="row hide">
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thedeepblue.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image" alt=""  data-src="media/artworks/thedeepblue.jpeg"/>
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thedeepblue-lazy.jpeg" data-src="media/artworks/thedeepblue.jpeg"/>
         </div>
       </a>
       <span><small><i>The Deep Blue</i> - oil on board - 30x30cms</small></span>
@@ -176,7 +176,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/amberdust.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image" alt=""  data-src="media/artworks/amberdust.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/amberdust-lazy.jpeg" data-src="media/artworks/amberdust.jpeg" />
         </div>
       </a>
       <span><small><i>Amber Dust</i> - oil on board - 30x30cms</small></span>
@@ -184,17 +184,17 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/remainsofthebird.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image" alt=""  data-src="media/artworks/remainsofthebird.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/remainsofthebird-lazy.jpeg" data-src="media/artworks/remainsofthebird.jpeg" />
         </div>
       </a>
       <span><small><i>Remains of the Bird</i> - oil on board - 30x30cms</small></span>
     </div>
   </div>
-  <div class="row hide">
+  <div class="row">
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thegardenatfarr.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image" alt="" data-src="media/artworks/thegardenatfarr.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thegardenatfarr-lazy.jpeg" data-src="media/artworks/thegardenatfarr.jpeg" />
         </div>
       </a>
       <span><small><i>The Garden at Farr</i> - oil on board - 40x40cms</small></span>
@@ -202,7 +202,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thelongview.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thelongview.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thelongview-lazy.jpeg" data-src="media/artworks/thelongview.jpeg" />
         </div>
       </a>
       <span><small><i>The Long View</i> - oil on canvas - 60x40cms</small></span>
@@ -210,7 +210,7 @@
     <div class="artwork col-md-4  col-xs-12">
       <a class="fancybox thumbnail" rel="lightbox" href="media/artworks/thewayahead.jpeg">
         <div class="image-container">
-          <img class="img-responsive uninitialised-image " alt="" data-src="media/artworks/thewayahead.jpeg" />
+          <img class="img-responsive lazy-image" alt="" src="media/artworks/thewayahead-lazy.jpeg" data-src="media/artworks/thewayahead.jpeg" />
         </div>
       </a>
       <span><small><i>The Way Ahead</i> - oil on board - 30x21cms</small></span>
--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    var image = document.getElementById('splashimg');
-    var revealer = document.getElementById("revealer");
+    const image = document.getElementById('splashimg');
+    // const revealer = document.getElementById("revealer");
     new simpleParallax(image);
     $(".fancybox").fancybox({
         openEffect: "none",
@@ -15,14 +15,33 @@ $(document).ready(function(){
         }
     });
 
-    if (revealer){
-        revealer.addEventListener("click", function(){
-            var uninitialisedImgs = $('.uninitialised-image');
-            for(var i = 0; i < uninitialisedImgs.length; ++i){
-                uninitialisedImgs[i].src = uninitialisedImgs[i].getAttribute('data-src');
-            }
-            $(".hide").removeClass("hide");
-            $("#revealerDiv").hide();
-        });
-    }
+    const artworks = document.querySelectorAll("img[data-src]");
+
+    const loadImg = function(entries, observer){
+        entries.forEach(function(entry){
+            if(!entry.isIntersecting) return;
+            entry.target.src = entry.target.dataset.src;
+            entry.target.addEventListener("load", function(){
+                entry.target.classList.remove("lazy-image");
+            });
+            observer.unobserve(entry.target);
+         });
+    };
+
+    const imgObserver = new IntersectionObserver(loadImg,{root:null, threshold:0});
+
+    artworks.forEach(img => imgObserver.observe(img));
+    
+
+
+    // if (revealer){
+    //     revealer.addEventListener("click", function(){
+    //         const uninitialisedImgs = $('.uninitialised-image');
+    //         for(let i = 0; i < uninitialisedImgs.length; ++i){
+    //             uninitialisedImgs[i].src = uninitialisedImgs[i].getAttribute('data-src');
+    //         }
+    //         $(".hide").removeClass("hide");
+    //         $("#revealerDiv").hide();
+    //     });
+    // }
 });
